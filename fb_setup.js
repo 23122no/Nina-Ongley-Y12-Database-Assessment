ceaeb68545b73bd3173786fa60e1ac8073649fe8
23122no <23122no@hvhs.school.nz>
2026-06-08 13:30:24 +1200

    firebase linked

---
+++ fb_setup.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGPmqiI3iQiztQR27LecvKafk_a1Acf9I",
+  authDomain: "nina-ongley-12comp-database.firebaseapp.com",
+  databaseURL: "https://nina-ongley-12comp-database-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nina-ongley-12comp-database",
+  storageBucket: "nina-ongley-12comp-database.firebasestorage.app",
+  messagingSenderId: "416944112129",
+  appId: "1:416944112129:web:56aabab4e41b30504ef600"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
