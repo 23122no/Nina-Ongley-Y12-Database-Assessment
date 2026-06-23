f9633a115da069401069cfade2b83170dd3c20be
23122no <23122no@hvhs.school.nz>
2026-06-23 15:15:06 +1200

    forEach begun to implement

--- gameSelection.js
+++ gameSelection.js
@@ -4,12 +4,13 @@ var userAge;
 var spaceInvadersScoresRead = false;
 
 function fb_readScores(game) {
+    console.log("fb_readScores , game = " + game)
     if (game == "spaceInvaders") {
         spaceInvadersScoresRead = true;
-    } else if (game == "geoDash") {
-        spaceInvadersScoresRead = false;
-        leaderboard.innerHTML = ""
     } else {
+        if (game == "geoDash") {
+            spaceInvadersScoresRead = false;
+        }
         leaderboard.innerHTML = ""
     }
     console.log("reading " + game + "high scores")
@@ -17,6 +18,32 @@ function fb_readScores(game) {
     firebase.database().ref("database/" + game).orderByChild("score").once("value", fb_displayHighScores, fb_error)
 }
 
+async function fb_displayHighScores(scores) {
+    console.log("fb_displayHighScores")
+    console.log(scores.val())
+    scores.forEach(fb_displayOneScore)
+}
+
+let currentScore;
+
+function fb_displayOneScore(scoreData) {
+    console.log("running fb_displayOneScore")
+    currentScore = scoreData.val()
+    var currentUID = scoreData.key
+    //var userData = await firebase.database().ref("database/users/" + currentUID).once("value")
+    firebase.database().ref("database/users/" + currentUID).once("value", fb_readUserScoreData, fb_error)
+    //var currentUserData = userData.val()
+    //console.log(currentUserData)
+    //leaderboard.innerHTML += "<img src='" + currentUserData["photo"] + "' width='30px' height='30px' >"
+    leaderboard.innerHTML += "<p>          " + currentUID + " got " + currentScore["score"] + " points. </p><br><br>";
+}
+
+function fb_readUserScoreData(child) {
+    console.log(currentScore["score"])
+    console.log(child.val())
+}
+
+/****** 
 async function fb_displayHighScores(scores) {
     console.log(scores.val())
     var score = scores.val()
@@ -32,6 +59,7 @@ async function fb_displayHighScores(scores) {
         fb_readScores("spaceInvaders");
     }
 }
+*******/
 
 async function fb_getUserData() {
     console.log(uid)
