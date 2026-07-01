50dc9e7f8a8cb7d35a19a0f38e694fb500400f6b
23122no <23122no@hvhs.school.nz>
2026-07-01 08:47:07 +0000

    css grid

--- javascript/compareScores.js
+++ javascript/compareScores.js
@@ -10,8 +10,8 @@ async function fb_compareScores(game, recentScore) {
         console.log(recentScore)
         console.log(-pastScore["score"])
         // If the user's score they just got is bigger than the old score, then replace the old score
-        alert("You got your high score! Your old score was "+-pastScore["score"]+" and your new score is "+recentScore+"!")
         if (recentScore > -pastScore["score"]) {
+            alert("You got your high score! Your old score was "+-pastScore["score"]+" and your new score is "+recentScore+"!")
             firebase.database().ref("database/" + game + "/" + uid + "/score").set(-recentScore);
         }
     }
